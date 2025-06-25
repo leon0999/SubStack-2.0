eb3ebc60d8fcd780a0111fa3e6467ab72fa5efce
JustDoit181 <justdoit@gmail.com>
2025-06-25 17:09:36 +0900

    Initial Commit

---
+++ SubStack/SubStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubStackApp.swift
+//  SubStack
+//
+//  Created by user on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SubStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
